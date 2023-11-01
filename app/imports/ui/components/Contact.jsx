97c82506c557a52787852d1fcdb4abc72a8a71df
Matthew Fallah (マシュー・ファラー) <107450287+moi-cest-matthew@users.noreply.github.com>
2023-10-31 17:20:26 -1000

    completed after submitting failed branches to wrong assignment

---
+++ app/imports/ui/components/Contact.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Card } from 'react-bootstrap';
+
+/** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
+const StuffItem = ({ contact }) => (
+  <Card className="h-100">
+    <Card.Img src={contact.image} style={{ width: '75px' }} />
+    <Card.Body>
+      <Card.Title>{contact.firstName} {contact.lastName}</Card.Title>
+      <Card.Subtitle>{contact.address}</Card.Subtitle>
+      <Card.Text>{contact.description}</Card.Text>
+    </Card.Body>
+  </Card>
+);
+
+// Require a document to be passed to this component.
+StuffItem.propTypes = {
+  contact: PropTypes.shape({
+    firstName: PropTypes.string,
+    lastName: PropTypes.string,
+    address: PropTypes.string,
+    image: PropTypes.string,
+    description: PropTypes.string,
+    // _id: PropTypes.string,
+  }).isRequired,
+};
+
+export default StuffItem;

cdc4a75f16b04ab577342c1eb982bb7e556508fd
Matthew Fallah (マシュー・ファラー) <107450287+moi-cest-matthew@users.noreply.github.com>
2023-11-03 22:06:15 -1000

    Finished in 17 minutes

--- app/imports/ui/components/Contact.jsx
+++ app/imports/ui/components/Contact.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
-const StuffItem = ({ contact }) => (
+const Contact = ({ contact }) => (
   <Card className="h-100">
     <Card.Img src={contact.image} style={{ width: '75px' }} />
     <Card.Body>
       <Card.Title>{contact.firstName} {contact.lastName}</Card.Title>
       <Card.Subtitle>{contact.address}</Card.Subtitle>
       <Card.Text>{contact.description}</Card.Text>
+      <Link to={`/edit/${contact._id}`}>Edit</Link>
     </Card.Body>
   </Card>
 );
 
 // Require a document to be passed to this component.
-StuffItem.propTypes = {
+Contact.propTypes = {
   contact: PropTypes.shape({
     firstName: PropTypes.string,
     lastName: PropTypes.string,
@@ -23,8 +25,8 @@ StuffItem.propTypes = {
     image: PropTypes.string,
     description: PropTypes.string,
     // owner: PropTypes.string,
-    // _id: PropTypes.string,
+    _id: PropTypes.string,
   }).isRequired,
 };
 
-export default StuffItem;
+export default Contact;
